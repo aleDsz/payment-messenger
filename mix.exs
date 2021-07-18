06f2bf04903433cbe1dfaa599f2c537e6c035f65
Alexandre de Souza <alexandre@aledsz.com.br>
2021-07-18 03:59:39 -0300

    configure credo, coveralls and git hooks

--- mix.exs
+++ mix.exs
@@ -1,12 +1,26 @@
 defmodule PaymentMessenger.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @repo "https://github.com/aledsz/payment-messenger"
+
   def project do
     [
       app: :payment_messenger,
-      version: "0.1.0",
+      name: "Payment Messenger",
+      description: "ISO-8583 TLV messages with Ecto validation",
+      version: @version,
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
+      package: package(),
+      docs: docs(),
       deps: deps()
     ]
   end
@@ -17,10 +31,28 @@ defmodule PaymentMessenger.MixProject do
     ]
   end
 
+  defp docs do
+    [
+      main: "PaymentMessenger",
+      source_ref: "v#{@version}",
+      source_url: @repo,
+      extras: ["README.md"]
+    ]
+  end
+
+  defp package do
+    [
+      links: %{github: @repo}
+    ]
+  end
+
   defp deps do
     [
       {:ecto, "~> 3.6", optional: true},
-      {:temporary_env, "~> 2.0", only: [:test]}
+      {:temporary_env, "~> 2.0", only: :test},
+      {:credo, "~> 1.5.0", optional: true, only: :test, runtime: false},
+      {:excoveralls, "~> 0.10", optional: true, only: :test},
+      {:git_hooks, "~> 0.5.0", optional: true, only: :test, runtime: false}
     ]
   end
 end
