132f831a24eb03a866f3eb1b9315767117e13aee
Alexandre de Souza <alexandre@aledsz.com.br>
2021-07-18 01:51:57 -0300

    :tada: first commit

---
+++ lib/payment_messenger.ex
@@ -0,0 +1,18 @@
+defmodule PaymentMessenger do
+  @moduledoc """
+  Documentation for `PaymentMessenger`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PaymentMessenger.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
